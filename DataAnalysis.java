c3020a388089c40f5c51800032d12d2b9a126e7c
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2019-11-07 00:13:32 +0000

    added basics to data analysis

--- DataAnalysis.java
+++ DataAnalysis.java
@@ -1,4 +1,20 @@
+import java.time.LocalDate;
+import java.util.TreeMap;
 
-public class DataAnalysis {
+
+public class DataAnalysis extends BookingSystem {
 	
+	public void getFinancialInfo(String timeFrame) {
+		
+		Object[][] temp = readDataFromFile("");
+		TreeMap<LocalDate, Double> A = new TreeMap<LocalDate, Double>();
+		int count = 0;
+		
+		// add if statement to get data from correct time period date > current date - day/month/year
+		while(count < temp.length) {
+			A.put((LocalDate)temp[count][4],  (double)temp[count][9]);
+		}
+		
+	}
+
 }
