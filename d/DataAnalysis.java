0a963fb0a74f5d51a66f76fb6e51a43c563dc3c0
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2019-11-08 00:28:49 +0000

    added getAverage to dataAnalysis and added rough code to checkin/checkout

---
+++ d/DataAnalysis.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.util.TreeMap;
+
+
+public class DataAnalysis  {
+	
+	public TreeMap<LocalDate, Double> getFinancialInfo(LocalDate start, LocalDate end, Object[][] data) {
+		
+		TreeMap<LocalDate, Double> dateTotalCost = new TreeMap<LocalDate, Double>();
+		int count = 0;
+		
+		// add if statement to get data from time period between start and end including start and end
+		while(count < data.length) {
+			
+			if(((LocalDate)data[count][4]).compareTo(end) >= 0 || ((LocalDate)data[count][4]).compareTo(start) <= 0) {
+				dateTotalCost.put((LocalDate)data[count][4],  (double)data[count][9]);
+			}
+		}
+		return dateTotalCost;
+	}
+	
+	public void getAverageCostPerRoom(LocalDate start, LocalDate end, Object[][] data) {
+		TreeMap<LocalDate, Double> dateTotalCost = getFinancialInfo(start, end, data);
+		int average = 0;
+		for(double totalCost :dateTotalCost.values()) {
+			average += totalCost;
+		}
+		average = average/dateTotalCost.size();
+	}
+}
