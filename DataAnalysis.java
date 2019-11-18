48b5e46c181ed1a8683d1b6e68f88dd02c9c16de
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2019-11-18 10:16:28 +0000

    Update DataAnalysis.java

--- DataAnalysis.java
+++ DataAnalysis.java
@@ -1,22 +1,23 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 
 public class DataAnalysis  {
 	
-	public TreeMap<LocalDate, Double> getFinancialInfo(LocalDate start, LocalDate end, Object[][] data) {
+	public TreeMap<LocalDate,ArrayList<Double>> getFinancialInfo(LocalDate start, LocalDate end, Object[][] data) {
 		
-		TreeMap<LocalDate, Double> dateTotalCost = new TreeMap<LocalDate, Double>();
+		TreeMap<LocalDate, ArrayList<Double>> dateBalance = new TreeMap<LocalDate, ArrayList<Double>>();
 		int count = 0;
 		
 		// add if statement to get data from time period between start and end including start and end
 		while(count < data.length) {
-			
-			if(((LocalDate)data[count][4]).compareTo(end) >= 0 || ((LocalDate)data[count][4]).compareTo(start) <= 0) {
-				dateTotalCost.put((LocalDate)data[count][4],  (Double)data[count][9]);
+			if(((LocalDate)data[count][4]).compareTo(end) <= 0 && ((LocalDate)data[count][4]).compareTo(start) >= 0 ) {
+				dateBalance.put(((LocalDate)data[count][4]), new ArrayList<Double>());
+				dateBalance.get((LocalDate)data[count][4]).add((double)data[count][9]);
 			}
 		}
-		return dateTotalCost;
+		return dateBalance;
 	}
 	
 	public double getAverageCostPerRoom(LocalDate start, LocalDate end, Object[][] data) {
