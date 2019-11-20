511372c8dc136907cca9563dffaba7dc85a24e33
Edward Lynch-Milner <18222021@studentmail.ul.ie>
2019-11-20 21:57:38 +0000

    Update DataAnalysis.java

--- DataAnalysis.java
+++ DataAnalysis.java
@@ -10,7 +10,12 @@ import java.util.TreeMap;
 
 
 public class DataAnalysis  {
-	
+		private ArrayList<Reservation> reservations; //Could probably use booking system reservations when given a hotel name the supervisor is signed in as
+		
+		public DataAnalysis(ArrayList<Reservation> reservations) {
+			this.reservations = reservations;
+		}
+		
 		public void writeDataToFile(String filePath, Object[][] data) {
 			File file = new File(filePath);
 			try (PrintWriter writer = new PrintWriter(file)) {
@@ -33,7 +38,7 @@ public class DataAnalysis  {
 		}
 		
 		public void writeFinacialInfoToFile(LocalDate start, LocalDate end,TreeMap<String, ArrayList<Reservation>> reservations, String hotelName) {
-			TreeMap<LocalDate,ArrayList<Double>> financialInfo = getFinancialInfo(start, end, reservations,hotelName);
+			TreeMap<LocalDate,ArrayList<Double>> financialInfo = getFinancialInfo(start, end);
 			
 			int rows = financialInfo.size();
 			int columns = 2;
@@ -57,40 +62,45 @@ public class DataAnalysis  {
 
 		String fileName = "/data/dataAnalysis/FinanacialInfo.csv";
 		String path = System.getProperty("user.dir") + fileName;
-		this.writeDataToFile(path, data);
+		writeDataToFile(path, data);
 	}
 
 	// use reInitialize and take treeMap from bookingSystem? 
 	// 
-	public TreeMap<LocalDate,ArrayList<Double>> getFinancialInfo(LocalDate start, LocalDate end,TreeMap<String, ArrayList<Reservation>> reservations, String hotelName) {
-		
-		ArrayList<Reservation> resHotel = reservations.get(hotelName);
+	public TreeMap<LocalDate,ArrayList<Double>> getFinancialInfo(LocalDate start, LocalDate end) {
 		
 		TreeMap<LocalDate, ArrayList<Double>> dateBalance = new TreeMap<LocalDate, ArrayList<Double>>();
-		for(Reservation r : resHotel) {
+		for(Reservation r : reservations) {
 			
 			if(r.getCheckinDate().compareTo(start) >= 0 && r.getCheckinDate().compareTo(end) <= 0) {
-				dateBalance.put(r.getCheckinDate(), new ArrayList<Double>());
-				(dateBalance.get(r.getCheckinDate())).add(r.getTotalCost().getAmountDue());
+				LocalDate checkin = r.getCheckinDate();
+				if (!dateBalance.containsKey(checkin)) {
+					dateBalance.put(r.getCheckinDate(), new ArrayList<Double>());
+				}
+				dateBalance.get(r.getCheckinDate()).add(r.getTotalCost().getAmountDue());
 			}
 		}
 		return dateBalance;
 	}
 	
-	public double getAverageCostPerRoom(LocalDate start, LocalDate end, Object[][] data) {
-		TreeMap<LocalDate, ArrayList<Double>> dateTotalCost = getFinancialInfo(start, end, data);
-	 	int average = 0;
-		/*for(double totalCost : ) {
-			average += totalCost;
-		}*/
-		average = average/dateTotalCost.size(); 
+	public double getAverageIncome(LocalDate start, LocalDate end) {
+		TreeMap<LocalDate, ArrayList<Double>> dateTotalCost = getFinancialInfo(start, end); //maybe a method here to read reservations from file and put them in arraylist? and pass to the method with hotelName also
+	 	double average = 0;
+	 	int numOfEarnings = 0; //Each arraylist is a list of reservations of that date, so dateTotalCost().size() just returns number of key-value mappings, not amount of payments
+		for(Map.Entry<LocalDate, ArrayList<Double>> e : dateTotalCost.entrySet()) {
+			numOfEarnings += e.getValue().size();
+			for (double d : e.getValue()) {
+				average += d;
+			}
+		}
+		average = average/(double)numOfEarnings; 
 		return average; 
 	}
 	
-	public double getTotalEarned(LocalDate start, LocalDate end, Object[][] data) {
-		TreeMap<LocalDate, ArrayList<Double>> dateTotalCost = getFinancialInfo(start, end, data);
+	public double getTotalEarned(LocalDate start, LocalDate end) {
+		TreeMap<LocalDate, ArrayList<Double>> dateTotalCost = getFinancialInfo(start, end);
 		int total = 0;
-		for(ArrayList<Double> totalCost :dateTotalCost.values()) {
+		for(ArrayList<Double> totalCost : dateTotalCost.values()) {
 			for(double amount : totalCost) {
 				total += amount;
 			}
@@ -98,7 +108,7 @@ public class DataAnalysis  {
 		return total;
 	}
 	
-	public TreeMap<String,ArrayList<Integer>> getOccupancyInfo(LocalDate start, LocalDate end, Object[][] data) {
+	public TreeMap<String,ArrayList<Integer>> getOccupancyInfo(LocalDate start, LocalDate end) {
 		
 		return null;
 	}
